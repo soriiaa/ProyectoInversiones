dfddfa14041c625e95b74a2d7f4efe45db81ea02
Alejandro <alex.soriaalcaraz@gmail.com>
2024-06-28 22:22:57 +0200

    Commit 17 - Creo las ventanas de Login, Registro...

--- src/Controlador/Main.java
+++ src/Controlador/Main.java
@@ -4,16 +4,22 @@ package Controlador;
 import Modelo.Modelo;
 import Vista.Vista;
 import Vista._00_InicioAplicacion;
+import Vista._01_InicioSesion;
+import Vista._02_Registro;
+import Vista._03_TutorialAplicacion;
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		Modelo miModelo = new Modelo();
-		Vista[] misVistas = new Vista[1];
+		Vista[] misVistas = new Vista[4];
 		Controlador miControlador = new Controlador();
 
 		misVistas[0] = new _00_InicioAplicacion();
+		misVistas[1] = new _01_InicioSesion();
+		misVistas[2] = new _02_Registro();
+		misVistas[3] = new _03_TutorialAplicacion();
 
 		miModelo.setVista(misVistas);
 		miControlador.setVista(misVistas);
---
+++ src/Vista/_02_Registro.java
@@ -0,0 +1,29 @@
+package Vista;
+
+import javax.swing.JFrame;
+
+import Controlador.Controlador;
+import Modelo.Modelo;
+
+public class _02_Registro extends JFrame implements Vista {
+	
+	private Controlador miControlador;
+	private Modelo miModelo;
+
+	private JFrame frame;
+	
+	public void setModelo(Modelo miModelo) {
+		this.miModelo = miModelo;
+	}
+	
+	public void setControlador(Controlador miControlador) {
+		this.miControlador = miControlador;
+	}
+
+	public _02_Registro() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
